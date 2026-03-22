c5025925453bba2df5b3ba6d0c7c6f81c3f55def
abhirup2000-dev <ghoshabhirup465@gmail.com>
2026-03-22 16:35:31 +0530

    route updates

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,13 +4,13 @@ const router = express.Router()
 
 const recordRoutes = require('../routes/recordRoute');
 // View Routes (EJS pages)
-const viewRoutes = require('./empRoute');
+const empRoutes = require('./empRoute');
 
 
 
 router.use('/records', recordRoutes);
 
-router.use('/', viewRoutes);
+router.use('/', empRoutes);
 
 
 
